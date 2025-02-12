92bee5cafa649af4b83abba5120b4a1c123aa224
Akashdeep <akashdeep2k4@gmail.com>
2025-02-12 19:22:26 +0530

    add Home component to fetch and display Pokémon data in a styled list

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Card from "../components/Card";
+
+const StyledHome = styled.div``;
+const StyledList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(min(100%, 208px), 1fr));
+  gap: clamp(20px, 3.125vw, 40px);
+  padding: clamp(20px, 3.125vw, 40px);
+`;
+
+const Home = () => {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [offset, setOffset] = useState(50);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`);
+      const data = await response.json();
+      setPokemonData(data.results);
+    };
+    fetchPokemonData();
+  }, [offset]);
+
+  return (
+    <StyledHome>
+      <StyledList>
+        {pokemonData.map((currentPokemon) => {
+          return <Card key={currentPokemon.name} pokemon={currentPokemon} />;
+        })}
+      </StyledList>
+    </StyledHome>
+  );
+};
+
+export default Home;
