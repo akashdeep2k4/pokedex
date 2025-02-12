a5401e9cf0d1357c5d6717731be2fe9b63bcba4d
Akashdeep <akashdeep2k4@gmail.com>
2025-02-12 17:02:31 +0530

    implement main application structure with routing, header, footer, and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
-function App() {
-  return <>Pokedex</>;
-}
+import styled from "styled-components";
+import GlobalStyle from "./styles/globalStyles";
+import { theme } from "./styles/theme";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import NotFound from "./pages/NotFound";
+
+const Main = styled.main`
+  min-width: 100%;
+  width: 100%;
+`;
+
+const Section = styled.section`
+  max-width: 1280px;
+  min-height: 100vh;
+  margin: 0 auto;
+  background-color: white;
+  padding: ${theme.sizes.clamp24};
+`;
+
+const App = () => {
+  return (
+    <Router>
+      <Main>
+        <GlobalStyle />
+        <Header />
+
+        <Section>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pokemon/:id" element={<Details />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Section>
+
+        <Footer />
+      </Main>
+    </Router>
+  );
+};
 
 export default App;
