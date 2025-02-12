70fa39a9cfd04947b096403009fc5bc26216b6eb
Akashdeep <akashdeep2k4@gmail.com>
2025-02-12 16:59:23 +0530

    add globalstyle and theme for styled-components

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from "styled-components";
+import { theme } from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Pokemon Solid";
+    src: url("../src/assets/fonts/PokemonSolid.ttf") format("truetype");
+  }
+  @font-face {
+    font-family: "Fredoka One";
+    src: url("../src/assets/fonts/FredokaOne-Regular.ttf") format("truetype");
+  }
+  @font-face {
+    font-family: "Montserrat";
+    src: url("../src/assets/fonts/Montserrat-Regular.ttf") format("truetype");
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    font-size: 1rem;
+    line-height: 1.6;
+    font-family: ${theme.fonts.body}; 
+    background-color: ${theme.colors.gray};
+    color: black;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+
+  button {
+    cursor: pointer;
+    background-color: transparent;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.js
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    dark: "#434a54",
+    gray: "#e6e9ed",
+    red: "#ed5564",
+    yellow: "#F7C843",
+  },
+  fonts: {
+    logo: `"Pokemon Solid"`,
+    heading: `"Fredoka One"`,
+    body: `"Montserrat"`,
+  },
+  sizes: {
+    small: "8px",
+    medium: "16px",
+    large: "24px",
+    xLarge: "32px",
+    clamp24: "clamp(20px, 3.125vw, 40px)",
+    clamp48: "clamp(40px, 3.125vw, 80px)",
+  },
+  listGrid: "repeat(auto-fill, minmax(min(100%, 208px), 1fr))",
+  transition: "all 0.3s ease",
+};
