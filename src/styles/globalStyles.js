b577d7246fc4fd05b5ff24f793846cdba0cc5d90
Akashdeep <akashdeep2k4@gmail.com>
2025-02-12 19:58:51 +0530

    add font-weight and font-style properties to global font faces for consistency

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -6,16 +6,22 @@ const GlobalStyle = createGlobalStyle`
     font-family: "Pokemon Solid";
     src: url("../src/assets/fonts/PokemonSolid.woff2") format("woff2"),
          url("../src/assets/fonts/PokemonSolid.ttf") format("truetype");
+    font-weight: normal;
+    font-style: normal;
   }
   @font-face {
     font-family: "Fredoka One";
     src: url("../src/assets/fonts/FredokaOne-Regular.woff2") format("woff2"),
          url("../src/assets/fonts/FredokaOne-Regular.ttf") format("truetype");
+    font-weight: normal;
+    font-style: normal;
   }
   @font-face {
     font-family: "Montserrat";
     src: url("../src/assets/fonts/Montserrat-Regular.woff2") format("woff2"),
          url("../src/assets/fonts/Montserrat-Regular.ttf") format("truetype");
+    font-weight: normal;
+    font-style: normal;
   }
 
   * {
