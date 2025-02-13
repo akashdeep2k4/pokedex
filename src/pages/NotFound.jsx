b8267402440999d98950c72c5b736d31b4e9d585
Akashdeep <akashdeep2k4@gmail.com>
2025-02-13 22:31:37 +0530

    enhance NotFound component with styled layout and navigation link to home

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const PageNotFound = styled.div`
+  width: 100%;
+  height: 100vh;
+  align-content: center;
+  text-align: center;
+`;
+const GotoHome = styled.p`
+  margin-top: 40px;
+  text-decoration: underline;
+`;
+
 const NotFound = () => {
   return (
-    <div>
-      <p>NotFound Component</p>
-    </div>
+    <PageNotFound>
+      <h2>Page Not Found</h2>
+      <GotoHome>
+        <Link to={"/"}>Go To Home</Link>
+      </GotoHome>
+    </PageNotFound>
   );
 };
 
