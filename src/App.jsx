3fc73ca34de014d9d2594df590118378a8f388f4
Akashdeep <akashdeep2k4@gmail.com>
2025-02-13 20:07:04 +0530

    refactor App component to simplify structure; remove styled components and Footer, update route parameter for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import styled from "styled-components";
 import GlobalStyle from "./styles/globalStyles";
-import { theme } from "./styles/theme";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 import NotFound from "./pages/NotFound";
 
-const Main = styled.main`
-  min-width: 100%;
-  width: 100%;
-`;
-
-const Section = styled.section`
-  max-width: 1280px;
-  min-height: 100vh;
-  margin: 0 auto;
-  background-color: white;
-  padding: ${theme.sizes.clamp24};
-`;
-
 const App = () => {
   return (
     <Router>
-      <Main>
-        <GlobalStyle />
-        <Header />
-
-        <Section>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/pokemon/:name" element={<Details />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Section>
-
-        <Footer />
-      </Main>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:querry" element={<Details />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 };
