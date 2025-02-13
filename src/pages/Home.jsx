b5fb68dc2d5bab6eef1a8ef5efda60a147809e18
Akashdeep <akashdeep2k4@gmail.com>
2025-02-13 22:01:56 +0530

    add loading and error handling to Home component; implement load more functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { theme } from "../styles/theme";
 import Header from "../components/Header";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
@@ -10,29 +11,73 @@ const StyledList = styled.ul`
   gap: clamp(20px, 3.125vw, 40px);
 `;
 
+const StyledButton = styled.button`
+  margin-top: ${theme.sizes.clamp24};
+  border: 4px solid ${theme.colors.red};
+  color: ${theme.colors.red};
+  font-size: 20px;
+  font-weight: bold;
+  padding: 16px 32px;
+  border-radius: 16px;
+  transition: ${theme.transition};
+  &:hover {
+    color: white;
+    background-color: ${theme.colors.red};
+  }
+`;
+
+const LoadingText = styled.p`
+  text-align: center;
+`;
+const ErrorText = styled.p`
+  text-align: center;
+  color: ${theme.colors.red};
+`;
+
 const Home = () => {
   const [pokemonData, setPokemonData] = useState([]);
   const [offset, setOffset] = useState(0);
+  const [count, setCount] = useState(0);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`);
-      const data = await response.json();
-      setPokemonData(data.results);
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`);
+        const data = await response.json();
+        setPokemonData(pokemonData.concat(data.results));
+        setCount(data.count);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchPokemonData();
   }, [offset]);
+
+  const handleLoadMore = () => {
+    if (offset + 50 < count) {
+      setOffset(offset + 50);
+    }
+  };
 
   return (
     <>
       <Header />
 
       <section>
-        <StyledList>
-          {pokemonData.map((currentPokemon) => {
-            return <Card key={currentPokemon.name} pokemon={currentPokemon} />;
-          })}
-        </StyledList>
+        {isLoading && <LoadingText>Loading...</LoadingText>}
+        {isError && <ErrorText>Error Fetching Data.</ErrorText>}
+        {pokemonData && !isError && !isLoading && (
+          <StyledList>
+            {pokemonData.map((currentPokemon) => {
+              return <Card key={currentPokemon.name} pokemon={currentPokemon} />;
+            })}
+          </StyledList>
+        )}
+        {!isLoading && !isError && <StyledButton onClick={handleLoadMore}>Load More</StyledButton>}
       </section>
 
       <Footer />
